0c10ff8bfce1b2226bc83082ee118371904a109b
tmurugesh <73613956+tmurugesh@users.noreply.github.com>
2021-01-28 14:44:04 -0800

    updated code

---
+++ sketch.js
@@ -0,0 +1,15 @@
+
+// Setup code goes here
+function setup() {
+  createCanvas(600, 400);
+  textSize(50);
+  textAlign(CENTER);
+ }
+
+
+// Draw code goes here
+function draw() {
+  background(200,60,56);
+  fill(23,100,200);
+  text("Hello World!", width/2, height/2);
+}
